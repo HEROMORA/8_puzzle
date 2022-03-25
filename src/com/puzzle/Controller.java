b6d0a86a6e8db2ec82fcc94f61c563c71aeee777
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2022-03-25 13:40:05 +0200

    finished gui

--- src/com/puzzle/Controller.java
+++ src/com/puzzle/Controller.java
@@ -7,15 +7,19 @@ import com.puzzle.algos.SearchAlgorithm;
 import com.puzzle.cost.CostType;
 import com.puzzle.view.MainView;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Controller {
     static MainView view;
     static SearchAlgorithm algo;
+    static ArrayList<Integer> traceback;
     public Controller(MainView view)
     {
         this.view = view;
+        this.traceback = new ArrayList<>();
         this.view.getStartBtn().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -43,25 +47,56 @@ public class Controller {
                 Timer.tick();
                 int res = algo.search();
                 long time = Timer.tock();
-                setOuts(algo,time);
+
+                if( res != -1 ) {
+                    System.out.println("Cost = "+algo.sol.getCost());
+                    if(algo.sol.getCost() < 1000)
+                        System.out.println(algo.sol.traceback());
+
+                    setOuts(algo,time);
+
+                    traceback = algo.sol.traceback();
+                    traceback.remove(0);
+                }else {
+                    System.out.println("Unsolvable.");
+                    setOutsNoSol(algo, time);
+                    JOptionPane.showMessageDialog(null, "Unable To Solve!");
+                }
 
                 System.out.println("Finished in "+time+" Seconds");
                 System.out.println("Search Depth = "+algo.searchDepth);
                 System.out.println("Nodes expanded = "+algo.expandedNodesCount);
 
-                if(res == -1) System.out.println("Unsolvable.");
-                else {
-                    System.out.println("Cost = "+algo.sol.getCost());
-                    if(algo.sol.getCost() < 1000)
-                        System.out.println(algo.sol.traceback());
-                }
+                if( view.getTraceBackEnable().isSelected()) {
+                    view.getNxtStepBtn().addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            if (!traceback.isEmpty()) {
+                                setSeq(traceback.get(0));
+                                traceback.remove(0);
+                            }
+                        }
+                    });
+                }else if (! traceback.isEmpty())
+                    setSeq(traceback.get(traceback.size() - 1));
+
             }
+
         });
+
+
     }
     public static void setOuts(SearchAlgorithm algo, long time){
 
         view.getTime().setText(String.valueOf(time)+" Seconds");
         view.getCost().setText(String.valueOf(algo.sol.getCost()));
+        view.getDepth().setText(String.valueOf(algo.searchDepth));
+        view.getNodesExp().setText(String.valueOf(algo.expandedNodesCount));
+
+    }
+    public static void setOutsNoSol(SearchAlgorithm algo, long time){
+
+        view.getTime().setText(String.valueOf(time)+" Seconds");
         view.getDepth().setText(String.valueOf(algo.searchDepth));
         view.getNodesExp().setText(String.valueOf(algo.expandedNodesCount));
 
@@ -75,4 +110,13 @@ public class Controller {
         }
         return Util.getSequenceFromArr(arr);
     }
+    public void setSeq(int seq)
+    {
+        int [] arr = Util.getArrSequence(seq);
+        for(int i = 0; i < 9; i++) {
+            this.view.getFields()[i].setText(String.valueOf(arr[i]));
+        }
+    }
+
+
 }
